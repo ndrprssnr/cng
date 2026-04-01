f1848a680b8df5b4a0d13be6e0b4d72a8b80135f
André Preußner <andre.preussner@sap.com>
2026-04-01 14:40:10 +0200

    soft buttons of phone hidden, when playing

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
+import * as NavigationBar from 'expo-navigation-bar';
 import GameScreen from './src/screens/GameScreen';
 
 export default function App() {
+  useEffect(() => {
+    NavigationBar.setVisibilityAsync('hidden');
+    NavigationBar.setBehaviorAsync('overlay-swipe');
+  }, []);
+
   return (
     <>
-      <StatusBar style="light" />
+      <StatusBar style="light" hidden />
       <GameScreen />
     </>
   );
